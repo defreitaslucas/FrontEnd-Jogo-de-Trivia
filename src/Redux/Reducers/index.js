e7718dbb00b553f0faa2f7c54a20797e66fd4235
LucasTapa <lucas.dfa@live.com>
2022-03-09 18:50:48 -0300

    requisito 2 100%

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import playerReducer from './PlayerReducer';
+import tokenReducer from './TokenReducer';
 
 const rootReducer = combineReducers({
   player: playerReducer,
-
+  token: tokenReducer,
 });
 
 export default rootReducer;
