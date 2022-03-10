e0c32facd09180f8d7c6d3a2db7ee995778994c8
LucasTapa <lucas.dfa@live.com>
2022-03-10 20:31:13 -0300

    requisito 5 e 6 100% feito

--- src/Components/GamerQuestions.js
+++ src/Components/GamerQuestions.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { MAGIC_NUMBER_05 } from '../services/api';
-import Loading from './Loading';
+
 class GamerQuestions extends Component {
   state = {
     counter: 0,
@@ -13,44 +13,61 @@ class GamerQuestions extends Component {
     const currentQuestion = questions[number];
     const { incorrect_answers: incorrectAnswers,
       correct_answer: correctAnswer } = currentQuestion;
-    const answers = incorrectAnswers?.map((incorrectAnswer, index) => {
-      const dataTestId = `wrong-answer-${index}`
+    const answers = incorrectAnswers.map((incorrectAnswer, index) => {
+      const dataTestId = `wrong-answer-${index}`;
       return (
-        <button type='button' key={index} data-testid={dataTestId}>{incorrectAnswer}</button>
-      ) 
-    })
-    answers.push(<button type='button' key={correctAnswer} data-testid='correct-answer'>{correctAnswer}</button>)
-    .sort(() => MAGIC_NUMBER_05 - Math.random());
+        <button
+          type="button"
+          key={ index }
+          data-testid={ dataTestId }
+        >
+          {incorrectAnswer}
+        </button>
+      );
+    });
+    answers.push(
+      <button
+        type="button"
+        key={ correctAnswer }
+        data-testid="correct-answer"
+      >
+        {correctAnswer}
+      </button>,
+    );
+    answers.sort(() => MAGIC_NUMBER_05 - Math.random());
+    return answers;
   }
 
   mountedQuestion = (number) => {
     const { questions } = this.props;
     console.log(questions[0]);
-    const options = [questions[number]?.incorrect_answers,
-      questions[number]?.correct_answer];
+    // const options = [questions[number].incorrect_answers,
+    //   questions[number]?.correct_answer];
     return (
       <div>
-        <h1 data-testid="question-category">{questions[number]?.category}</h1>
-        <p data-testid="question-text">{questions[number]?.question}</p>
-        <div>{this.generateAnswers(number)}</div>
+        <h1 data-testid="question-category">{questions[number].category}</h1>
+        <p data-testid="question-text">{questions[number].question}</p>
+        <div data-testid="answer-options">{this.generateAnswers(number)}</div>
       </div>
     );
   }
 
   render() {
     const { counter } = this.state;
-
+    const { questions } = this.props;
     return (
       <div>
-        {this.mountedQuestion(counter) }
+        {
+          questions.length && this.mountedQuestion(counter)
+        }
       </div>
     );
   }
 }
 
 GamerQuestions.propTypes = {
-  questions: PropTypes.any
-}
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 const mapStateToProps = (state) => ({
   questions: state.questions,
