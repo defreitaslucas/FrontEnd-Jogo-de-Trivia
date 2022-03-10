64c8774214abf937c8ac405842764053367e707e
Felippe Fernandes <felippe1000@hotmail.com>
2022-03-10 12:28:26 -0300

    Teste requisito 5 incompleto

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,21 @@
 const API_TOKEN = 'https://opentdb.com/api_token.php?command=request';
 
 export const getTokenApi = async () => {
+  // const tokenStorage = JSON.parse(localStorage.getItem('token'));
   const request = await fetch(API_TOKEN);
   const requestJson = await request.json();
+  localStorage.setItem('token', JSON.stringify(requestJson));
   return requestJson;
 };
 
-export const fetchApiToken = async () => {
-  const token = await getTokenApi();
-  localStorage.setItem('token', token.token);
-  return token.token;
-};
-
 export const getQuestions = async () => {
-  const tokenStorage = localStorage.getItem('token');
-  const API_QUESTION = `https://opentdb.com/api.php?amount=5&token=${tokenStorage}`;
+  const tokenStorage = JSON.parse(localStorage.getItem('token'));
+  const API_QUESTION = `https://opentdb.com/api.php?amount=5&token=${tokenStorage.token}`;
   const questions = await fetch(API_QUESTION);
   const resultado = await questions.json();
+  if (!resultado.results.length) {
+    getTokenApi();
+    return resultado;
+  }
   return resultado;
 };
